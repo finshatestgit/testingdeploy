916eedf26179a88d32c17417c28e854413a3f172
finshatestgit <150815800+finshatestgit@users.noreply.github.com>
2023-11-16 01:07:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,15 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/dialogflow-webhook', methods=['POST'])
-def test_endpoint():
-    return jsonify({"message": "Endpoint is working!"})
+def dialogflow_webhook():
+    data = request.get_json()
+    intent_name = data['queryResult']['intent']['displayName']
+
+    # Handling only the default welcome intent
+    if intent_name == "Default Welcome Intent":
+        return jsonify({"fulfillmentText": "Welcome! This is a test response."})
+    else:
+        return jsonify({"fulfillmentText": "Sorry, I can only respond to the welcome intent in this test version."})
 
 if __name__ == "__main__":
     app.run(debug=True)
